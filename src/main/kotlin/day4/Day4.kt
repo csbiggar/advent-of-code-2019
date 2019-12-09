65ac3432f54ed342b7f9b85e72b1493e67875511
Carolyn Biggar <carolyn.biggar@johnlewis.co.uk>
2019-12-09 23:25:19 +0000

    Day 4 Part 2 - Must contain an exact double, not more repetitions

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -4,12 +4,12 @@ fun main() {
     val result = (240298..784956)
         .filter { meetsCriteria(it) }
         .count()
-
+    
     println(result)
 }
 
 fun meetsCriteria(number: Int, min: Int = 240298, max: Int = 784956): Boolean {
-    return containsDouble(number)
+    return containsExactDouble(number)
             && isAscending(number)
             && number in min..max
 }
@@ -24,11 +24,31 @@ private fun isAscending(number: Int): Boolean {
     return true
 }
 
-private fun containsDouble(number: Int): Boolean {
-    return number
-        .toString()
-        .windowed(2, 1)
-        .filter { it.length == 2 }
-        .any { neighbours -> neighbours[0] == neighbours[1] }
+private fun containsExactDouble(number: Int): Boolean {
+    val digits = number.toString().toList().map { it.toInt() }
+    val lastIndex = digits.size - 1
+
+    (0..lastIndex).forEach { index ->
+        if (pairOfDigitsMatch(digits, index)
+            && digitBeforePairIsDifferent(digits, index)
+            && digitAfterPairIsDifferent(digits, index)
+        ) return true
+    }
+    return false
 }
 
+private fun digitAfterPairIsDifferent(digits: List<Int>, index: Int): Boolean {
+    val lastIndex = digits.size - 1
+    val indexBeyondNext = index + 2
+    return indexBeyondNext > lastIndex || digits[indexBeyondNext] != digits[index]
+}
+
+private fun digitBeforePairIsDifferent(digits: List<Int>, index: Int): Boolean {
+    val previousIndex = index - 1
+    return previousIndex < 0 || digits[previousIndex] != digits[index]
+}
+
+private fun pairOfDigitsMatch(digits: List<Int>, index: Int): Boolean {
+    val lastIndex = digits.size - 1
+    return index < lastIndex && digits[index] == digits[index + 1]
+}
--- src/test/kotlin/day4/Day4Test.kt
+++ src/test/kotlin/day4/Day4Test.kt
@@ -3,7 +3,7 @@ package day4
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-internal class Day4Test {
+class Day4Test {
 
     @Test
     fun `numbers without doubles do not meet the criteria`() {
@@ -12,7 +12,7 @@ internal class Day4Test {
     }
 
     @Test
-    fun `numbers with doubles do not meet the criteria`() {
+    fun `numbers with doubles meet the criteria`() {
         val result = meetsCriteria(number = 1224, min = 1111, max = 9999)
         assertThat(result).isTrue()
     }
@@ -34,4 +34,17 @@ internal class Day4Test {
         val result = meetsCriteria(number = 88999, min = 1111, max = 9999)
         assertThat(result).isFalse()
     }
+
+    @Test
+    fun someTest() {
+        val result1 = meetsCriteria(number = 112233, min = 1, max = 9999999)
+        assertThat(result1).isTrue()
+
+        val result2 = meetsCriteria(number = 123444, min = 1, max = 99999999)
+        assertThat(result2).isFalse()
+
+        val result3 = meetsCriteria(number = 111122, min = 1, max = 99999999)
+        assertThat(result3).`as`("this should really work").isTrue()
+    }
+
 }
