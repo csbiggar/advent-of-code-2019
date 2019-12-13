b68cc69a51a097b8e32bca964c8afd3c49ff5ade
Carolyn Biggar <carolyn.biggar@johnlewis.co.uk>
2019-12-13 01:12:24 +0000

    Day 6 Part 2 - Possibly works, but takes too long to run
    Gave up after a few minutes, either stuck in an infinite loop
    or taking more than 5 min to run!

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -1,31 +1,45 @@
 package day6
 
+import FileReader
+import day6.SpaceObject.Companion.SANTA
+import day6.SpaceObject.Companion.YOU
+
 fun main() {
     val orbitsSpec = FileReader.readLines("day6/orbits.txt").joinToString(",")
     println(countOrbits(orbitsSpec))
+    println(transfersRequired(orbitsSpec))
 }
 
 
 fun countOrbits(orbitsSpec: String): Int {
-    val allOrbits = orbitsSpec
-        .split(",")
-        .map {
-            Orbit(
-                orbiter = SpaceObject(it.substringAfter(")")),
-                orbits = SpaceObject(it.substringBefore(")"))
-            )
-        }
+    val allOrbits = mapOrbits(orbitsSpec)
 
-    val distinctSpaceObjects = (allOrbits.map { it.orbiter } + allOrbits.map { it.orbits }).distinct()
+    val distinctSpaceObjects = (allOrbits.map { it.orbitedBy } + allOrbits.map { it.orbitedObject }).distinct()
 
     return descendents(distinctSpaceObjects, allOrbits).count() - 1
 }
 
+private fun mapOrbits(orbitsSpec: String): List<Orbit> {
+    return orbitsSpec
+        .split(",")
+        .map {
+            Orbit(
+                orbitedBy = SpaceObject(it.substringAfter(")")),
+                orbitedObject = SpaceObject(it.substringBefore(")"))
+            )
+        }
+}
 
 private fun findDirectOrbits(spaceObject: SpaceObject, allOrbits: List<Orbit>): List<SpaceObject> {
     return allOrbits
-        .filter { it.orbiter == spaceObject }
-        .map { it.orbits }
+        .filter { it.orbitedBy == spaceObject }
+        .map { it.orbitedObject }
+}
+
+private fun findDirectParents(spaceObject: SpaceObject, allOrbits: List<Orbit>): List<SpaceObject> {
+    return allOrbits
+        .filter { it.orbitedObject == spaceObject }
+        .map { it.orbitedBy }
 }
 
 private fun descendents(spaceObjects: List<SpaceObject>, allOrbits: List<Orbit>): List<SpaceObject> {
@@ -35,5 +49,34 @@ private fun descendents(spaceObjects: List<SpaceObject>, allOrbits: List<Orbit>)
 
 }
 
-data class Orbit(val orbiter: SpaceObject, val orbits: SpaceObject)
-data class SpaceObject(val name: String)
+data class Orbit(val orbitedBy: SpaceObject, val orbitedObject: SpaceObject)
+data class SpaceObject(val name: String) {
+    companion object {
+        val SANTA = SpaceObject("SAN")
+        val YOU = SpaceObject("YOU")
+    }
+}
+
+fun transfersRequired(orbitsSpec: String): Int {
+    val allOrbits = mapOrbits(orbitsSpec)
+
+    val santaIsOrbiting = allOrbits.first { it.orbitedBy == SANTA }.orbitedObject
+
+    return findTransfers(listOf(YOU), santaIsOrbiting, allOrbits)
+}
+
+private fun findTransfers(
+    spaceObjects: List<SpaceObject>,
+    santaIsOrbiting: SpaceObject,
+    allOrbits: List<Orbit>,
+    transferCount: Int = 0
+): Int {
+    val orbits = spaceObjects.flatMap { findDirectOrbits(it, allOrbits) }
+    val orbitedBy = spaceObjects.flatMap { findDirectParents(it, allOrbits) }
+
+    return if (santaIsOrbiting in orbits || santaIsOrbiting in orbitedBy) {
+        transferCount
+    } else {
+        findTransfers(orbits + orbitedBy, santaIsOrbiting, allOrbits, transferCount + 1)
+    }
+}
--- src/test/kotlin/day6/Day6Test.kt
+++ src/test/kotlin/day6/Day6Test.kt
@@ -1,7 +1,6 @@
 package day6
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 internal class Day6Test {
@@ -27,8 +26,42 @@ internal class Day6Test {
     }
 
     @Test
-    fun `longer example`() {
+    fun `total orbits longer example`() {
         assertThat(countOrbits("COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L")).isEqualTo(42)
     }
 
+    @Test
+    fun `no hops to santa`() {
+        assertThat(transfersRequired("I)YOU,I)SAN")).isEqualTo(0)
+    }
+
+    @Test
+    fun `one hop to santa`() {
+        assertThat(transfersRequired("I)K,K)YOU,I)SAN")).isEqualTo(1)
+    }
+
+    @Test
+    fun `two hops to santa`() {
+        assertThat(transfersRequired("I)J,J)K,K)YOU,I)SAN")).isEqualTo(2)
+    }
+
+    @Test
+    fun `two hops to santa the other way`() {
+        assertThat(transfersRequired("J)I,K)J,K)YOU,I)SAN")).isEqualTo(2)
+    }
+
+    @Test
+    fun `one hop to santa the other way`() {
+        assertThat(transfersRequired("K)I,K)YOU,I)SAN")).isEqualTo(1)
+    }
+
+    @Test
+    fun `you to santa`() {
+        assertThat(transfersRequired("COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L,K)YOU,I)SAN")).isEqualTo(4)
+    }
+
 }
+
+
+
+
