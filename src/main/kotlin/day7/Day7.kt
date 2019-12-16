127ffacc3896df3dc5c9b8af9dda69a0571e103a
Carolyn Biggar <carolyn.biggar@johnlewis.co.uk>
2019-12-16 23:19:39 +0000

    Day 7 Part 1 - Calculate thrust signal for a given phase sequence

--- src/main/kotlin/day7/Day7.kt
+++ src/main/kotlin/day7/Day7.kt
@@ -3,10 +3,13 @@ package day7
 import intcode.Program
 
 
-//fun thrust(programInput: String, phaseSequence: List<Int>): Int {
-//    var output: Int
-//
-//    for (input in phaseSequence) {
-//        Program(programInput).run(input)
-//    }
-//}
+fun calculateThrustSignal(programInput: String, phaseSequence: List<Int>): Int {
+    var nextInput = 0
+
+    for (phase in phaseSequence) {
+        nextInput = Program(programInput).run(phase, nextInput)
+            ?: throw IllegalStateException("What do you mean, there's no output?")
+    }
+
+    return nextInput
+}
