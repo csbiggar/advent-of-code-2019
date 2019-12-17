ee4fb2fc809520f6badd3cce0b9549d5df650418
Carolyn Biggar <carolyn.biggar@johnlewis.co.uk>
2019-12-17 00:29:04 +0000

    Day 7 Part 1 - Find max thruster signal

--- src/main/kotlin/day7/Day7.kt
+++ src/main/kotlin/day7/Day7.kt
@@ -1,9 +1,16 @@
 package day7
 
+import FileReader
+import com.marcinmoskala.math.permutations
 import intcode.Program
 
+fun main() {
+    val programInput = FileReader.readFile("day7/amplifier-software.csv")
+    val result = calculateMaxThrusterSignal(programInput)
+    println("Max possible thruster signal: $result")
+}
 
-fun calculateThrustSignal(programInput: String, phaseSequence: List<Int>): Int {
+fun calculateThrusterSignal(programInput: String, phaseSequence: List<Int>): Int {
     var nextInput = 0
 
     for (phase in phaseSequence) {
@@ -12,4 +19,15 @@ fun calculateThrustSignal(programInput: String, phaseSequence: List<Int>): Int {
     }
 
     return nextInput
+}
+
+fun calculateMaxThrusterSignal(programInput: String): Int {
+    val results = mutableListOf<Int>()
+
+    for (phaseSequence in listOf(0, 1, 2, 3, 4).permutations()) {
+        val thrusterSignal = calculateThrusterSignal(programInput, phaseSequence)
+        results.add(thrusterSignal)
+    }
+
+    return results.max() ?: throw java.lang.IllegalStateException("There should be some results here...")
 }
--- src/test/kotlin/day7/Day7Test.kt
+++ src/test/kotlin/day7/Day7Test.kt
@@ -7,21 +7,51 @@ internal class Day7Test {
 
     @Test
     fun `example 1 `() {
-        val result = calculateThrustSignal("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0", listOf(4,3,2,1,0))
+        val result = calculateThrusterSignal("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0", listOf(4, 3, 2, 1, 0))
         assertThat(result).isEqualTo(43210)
     }
 
     @Test
     fun `example 2 `() {
-        val result = calculateThrustSignal("3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0", listOf(0,1,2,3,4))
+        val result = calculateThrusterSignal(
+            "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0",
+            listOf(0, 1, 2, 3, 4)
+        )
         assertThat(result).isEqualTo(54321)
     }
 
     @Test
     fun `example 3 `() {
         val result =
-            calculateThrustSignal("3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0", listOf(1,0,4,3,2))
+            calculateThrusterSignal(
+                "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0",
+                listOf(1, 0, 4, 3, 2)
+            )
         assertThat(result).isEqualTo(65210)
     }
 
+    @Test
+    fun `should find max for example 1`(){
+        val result = calculateMaxThrusterSignal("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0")
+        assertThat(result).isEqualTo(43210)
+    }
+
+    @Test
+    fun `should find max for example 2`(){
+        val result = calculateMaxThrusterSignal("3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0")
+        assertThat(result).isEqualTo(54321)
+    }
+
+    @Test
+    fun `should find max for example 3`(){
+        val result = calculateMaxThrusterSignal("3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0")
+        assertThat(result).isEqualTo(65210)
+    }
+
+    @Test
+    fun `should calculate real Part 1 max thruster`(){
+        val programInput = FileReader.readFile("day7/amplifier-software.csv")
+        val result = calculateMaxThrusterSignal(programInput)
+        assertThat(result).isEqualTo(929800)
+    }
 }
